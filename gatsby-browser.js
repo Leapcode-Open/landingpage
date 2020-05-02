711a079a3a74628693305b8eb7b2671a4693b710
Athul Suresh <atulsuresh123@gmail.com>
2020-05-02 19:33:33 +0530

    survey

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,3 +6,5 @@
 
 // You can delete this file if you're not using it
 import './src/assets/mail.css';
+
+// (function(t,e,s,o){var n,c,l;t.SMCX=t.SMCX||[],e.getElementById(o)||(n=e.getElementsByTagName(s),c=n[n.length-1],l=e.createElement(s),l.type="text/javascript",l.async=!0,l.id=o,l.src=["https:"===location.protocol?"https://":"http://","widget.surveymonkey.com/collect/website/js/tRaiETqnLgj758hTBazgd_2BcEYo8O3z9VnKhrJB6wgHJnsLHvfrE5KV95XWNat2rU.js"].join(""),c.parentNode.insertBefore(l,c))})(window,document,"script","smcx-sdk")
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,49 @@
-import React from "react"
+import React, { Component } from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
-import SEO from "../components/seo"
+import SEO from "../components/seo";
 
-const IndexPage = () => (
+
+
+const SurveyModel = ({ isOpen }) => (
+  <div class={`modal ${ isOpen ? 'opacity-100' : 'pointer-events-none none opacity-0'}  fixed w-full h-full top-0 left-0 flex items-center justify-center z-30`}>
+    <div class="modal-overlay absolute w-full h-full bg-gray-900 opacity-50"></div>
+    <div class="modal-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
+      <div class="modal-content bg-white py-4   text-left w-full mx-auto">
+        <div id="smcx-sdk" className="z-40" />    
+      </div>
+    </div>
+   
+  </div>
+)
+
+class IndexPage extends Component {
+
+  componentDidMount() {
+    let el = document.createElement("script");
+    el.src = "https://widget.surveymonkey.com/collect/website/js/tRaiETqnLgj758hTBazgd_2BcEYo8O3z9VnKhrJB6wgHLJnAQLYKUVLpQF4kbKBOIK.js";
+    document.body.appendChild(el);
+  }
+
+  state = {
+    sur: false
+  }
+
+  runSurvey = () => {
+    console.log('ssd')
+    this.setState({
+      sur: true
+    })
+  }
+  
+  render() {
+    const props = this.props;
+    return(
   <Layout>
     <SEO title="Leapcode Open" />
+    <SurveyModel isOpen={this.state.sur} />
     <div className="w-screen min-h-screen flex justify-center items-center flex-col">
       <div className="logo md:w-1/2 lg:w-1/2 mb-10">
         <img className="md:w-1/4 lg:w-1/3 w-1/2 mx-auto" src={require('../images/leapcode--open--noslash.png')} />
@@ -16,10 +52,9 @@ const IndexPage = () => (
         <h1 className="font-medium dark:text-white font-gt font-bold tracking-tighter text-gray-900 leading-10">A better way to get started with <br></br>open source contribution</h1>
         <p className='dark:text-white font-medium font-gt'>We're building Leapcode Open to help you get things rolling with your first open source contribution</p>
         <p className='dark:text-white font-medium text-gray-800 font-gt mt-8 block'>Be the first to know!</p> 
-        <a href="#" className=' inline-block px-4 py-3 bg-purple-700 hover:bg-purple-800 text-white font-gt rounded text-sm' style= {{ backgroundColor:"#ed4857"}} >Count me in🙋‍♀️</a>  
+        <a onClick={this.runSurvey} className=' inline-block px-4 py-3 bg-purple-700 hover:bg-purple-800 text-white font-gt rounded text-sm' style= {{ backgroundColor:"#ed4857"}} >Count me in🙋‍♀️</a>  
       </div>
     </div>
-    {/* <script>(function(t,e,s,o){var n,c,l;t.SMCX=t.SMCX||[],e.getElementById(o)||(n=e.getElementsByTagName(s),c=n[n.length-1],l=e.createElement(s),l.type="text/javascript",l.async=!0,l.id=o,l.src=["https:"===location.protocol?"https://":"http://","widget.surveymonkey.com/collect/website/js/tRaiETqnLgj758hTBazgd_2BcEYo8O3z9VnKhrJB6wgHJnsLHvfrE5KV95XWNat2rU.js"].join(""),c.parentNode.insertBefore(l,c))})(window,document,"script","smcx-sdk");</script> */}
     <section className=" bg-gray-100 py-4">
         <div className="md:w-1/2 px-4 mx-auto text-lg leading-8 py-4 my-4" >
           <p className="font-gt py-4 my-4">You have read a lot of blogs on the internet & went through different resources on contributing to open source. But, still stuck and haven't made any contributions yet.<br /><br />
@@ -32,13 +67,18 @@ But still, we can make it more comfortable and exciting for you to get started. 
     </p>
         </div>
     </section>
+ 
 
 
 
     <div className="font-gt md:w-1/2 px-4 mx-auto mt-4 py-4 text-gray-500">
       From the <a href="https://leapcode.io">Leapcode</a> team
     </div>
+
+ 
+
   </Layout>
-)
+)}}
+
 
 export default IndexPage
