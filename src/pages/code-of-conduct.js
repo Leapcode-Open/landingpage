c43ef08b8a69e9679f6cf708e6ecca33787e7247
Sethu Sathyan <sethusathyan24@gmail.com>
2020-09-12 13:19:53 +0530

    Update code-of-conduct.js

--- src/pages/code-of-conduct.js
+++ src/pages/code-of-conduct.js
@@ -96,7 +96,7 @@ a project may be further defined and clarified by project maintainers.</p>
 <h5 className="text-2xl font-gt text-newblue-900">Enforcement</h5>
                 <p className="text-base font-gt  text-newblue-900 tracking-wide">
                 Instances of abusive, harassing, or otherwise unacceptable behavior may be
-reported by contacting the project team at sethu@leapcode.io. All
+reported by contacting the project team. All
 complaints will be reviewed and investigated and will result in a response that
 is deemed necessary and appropriate to the circumstances. The project team is
 obligated to maintain confidentiality with regard to the reporter of an incident.
