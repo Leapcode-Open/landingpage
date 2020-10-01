f9fd0a3f232a33b6d681f976dbaceb66c1fa600b
Athul Suresh <atulsuresh123@gmail.com>
2020-10-01 19:52:46 +0530

    fixed contributor link (#140)

--- src/pages/contributor.js
+++ src/pages/contributor.js
@@ -26,7 +26,7 @@ class Contributor extends Component {
                 <div className="max-w-screen-lg mx-auto px-4 py-4">
                     <h1 className="text-2xl font-gt">Contributor on Leapcode</h1>
                     <div className="mt-12">
-                         { shuffleArray(this.props.data.allMarkdownRemark.edges).map(contrib => (
+                         { this.props.data.allMarkdownRemark.edges.map(contrib => (
                              <a key={contrib.node.frontmatter.username} href={`/contributor/${contrib.node.frontmatter.username}`} className="p-3 border border-gray-300 rounded-lg mb-4 block bg-white shadow-sm hover:shadow-lg">
                                     <h3 className="font-gt text-base mb-1">{contrib.node.frontmatter.name}</h3>
                                     <small className="font-gt text-gray-600 text-xs mb-1 block">{contrib.node.frontmatter.username}</small>
